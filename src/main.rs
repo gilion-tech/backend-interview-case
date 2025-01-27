85229b8654276a3b1f6e4a498536211f51b83d25
Joel W. Blomster <joel.blomster@gilion.com>
2025-01-27 13:14:24 +0100

    Built a proper streaming response

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,63 @@ use std::convert::Infallible;
 
 use axum::{
     body::{Body, Bytes},
-    http::Response,
+    http::StatusCode,
+    response::Response,
     routing::get,
     Router,
 };
-use chrono::{Duration, NaiveDate};
-use futures::stream::{self, StreamExt};
+use futures::{stream, StreamExt};
 use rand::Rng;
 
-fn gen_random_date() -> NaiveDate {
-    let end = NaiveDate::from_ymd_opt(2025, 1, 1).unwrap();
-    let start = NaiveDate::from_ymd_opt(1950, 1, 1).unwrap();
-    let days_in_range = (end - start).num_days();
-    let random_days: i64 = Rng::gen_range(&mut rand::thread_rng(), 0..days_in_range);
-    return start + Duration::days(random_days);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/api/v1/data-stream", get(get_data_stream));
+
+    let addr = "localhost:3000";
+    let listener = tokio::net::TcpListener::bind(&addr).await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
 
-async fn get_data() -> Response<Body> {
-    let data_stream = stream::iter(0..100).map(|num| {
-        // Convert each number to a `Bytes` object
-        let bytes = Bytes::from(format!("{num},"));
-        Ok(bytes) as Result<Bytes, Infallible>
+async fn root() -> &'static str {
+    "
+        Welcome to Quantum Innovations Inc.
+
+        We are a company that specializes in quantum computing and data science.
+        We are currently working on a project that involves generating synthetic data.
+        This project is still in the early stages, but we are making progress.
+        Our goal is to create a data stream that can be used for training forecasting models.
+        But before we can do that, we need a middle layer which feeds the data to our visualizer app.
+
+        The data stream can be accessed at /api/v1/data-stream
+    "
+}
+
+async fn get_data_stream() -> Response<Body> {
+    let stream = stream::iter(0..1_000_000_000).map(|_| {
+        let random_date = get_random_data();
+        Ok::<_, Infallible>(random_date)
     });
 
     Response::builder()
-        .status(200)
-        .header("Content-Type", "application/json")
-        .body(Body::from_stream(data_stream))
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/octet-stream")
+        .body(Body::from_stream(stream))
         .unwrap()
 }
 
-
-#[tokio::main]
-async fn main() {
-    let app = Router::new()
-        .route("/data", get(get_data));
+fn get_random_data() -> axum::body::Bytes {
+    let mut rng = rand::thread_rng();
+    let random_date = format!(
+        "{:04}{:02}{:02}{:03}{:03}",
+        rng.gen_range(1960..=2025),
+        rng.gen_range(1..=12),
+        rng.gen_range(1..=31),
+        rng.gen_range(0..=255),
+        rng.gen_range(0..=255)
+    );
+    Bytes::from(random_date)
 }
-
-
